75a7fabd4758317fd646beae42250c9d29d15c36
Ansh-Agrawal-17112003 <anshagrawal944@gmail.com>
2026-05-26 19:21:58 +0000

    container config commit

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,10 @@
+const accountID=143224
+let accountEmail="anshagrawal@gmail.com"
+var accountPassword="432421"
+accountCity="jaipur"
+
+accountEmail="ansh@gamil.com"
+accountPassword="121212"
+accountCity="bengaluru"
+console.table([accountID,accountEmail,accountPassword,accountCity])
+
